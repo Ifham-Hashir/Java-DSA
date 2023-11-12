b0da5c9c1847d178d2513933cb9b1cdaabc3e774
Ifham-Hashir <ifham09052003@gmail.com>
2023-11-12 09:30:26 +0530

    Add MaxSubarraySum-III (Kadanes Algorithm)

---
+++ 1.Arrays/KadanesAlgo.java
@@ -0,0 +1,25 @@
+//Also works for all negative elements in array
+public class KadanesAlgo {
+    public static void kadanes_algorithm(int[] arr){
+        int maxSum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for(int i = 0; i < arr.length; i++){
+            currSum = currSum + arr[i];
+
+            if(maxSum < currSum){
+                maxSum = currSum;
+            }
+
+            if(currSum < 0){
+                currSum = 0;
+            }
+        }
+        System.out.println("Maximum Subarray Sum = " + maxSum);
+    }
+    public static void main(String[] args) {
+        int[] arr = {-1,-2,6,-1,3};
+        kadanes_algorithm(arr);
+    }
+}
+
+/* Output: Maximum Subarray Sum = 8 */
